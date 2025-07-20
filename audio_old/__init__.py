22057c638bdd0523037bd249bde000639b819598
Ayşe Kaya <113442948+vzynszice@users.noreply.github.com>
2025-07-20 19:30:52 +0300

    Added old version of audio/__init__.py

---
+++ audio_old/__init__.py
@@ -0,0 +1,10 @@
+from .audio_recorder import AudioRecorder
+from .audio_player import AudioPlayer
+from .screen_recorder import ScreenRecorder
+
+__all__ = [
+    'AudioRecorder',
+    'AudioPlayer', 
+    'ScreenRecorder'
+]
+__version__ = '1.0.0'
